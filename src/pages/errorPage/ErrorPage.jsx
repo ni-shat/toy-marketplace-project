e41503d17fa5d4b51695c5b28deeb78d1e1c935a
ni-shat <nishat.tabassum301@gmail.com>
2023-05-21 13:51:53 +0600

    404 page is added

--- src/pages/errorPage/ErrorPage.jsx
+++ src/pages/errorPage/ErrorPage.jsx
@@ -1,10 +1,18 @@
-
+import error from '../../assets/error/err.png';
+import four from '../../assets/error/404.png';
+import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
     return (
-        <div>
-            error
-        </div>
+        <>
+            <div className='flex justify-center -mt-14 items-center '>
+                <img className='w-[43%]' src={four} alt="" />
+
+            </div>
+            <div className='flex justify-center items-center mt-14 text-3xl font-normal border border-[#7AC3C4] rounded-full px-10 py-5 w-fit mx-auto'>
+                <Link to='/'>Go Back to <span className='text-[#64a0a1] font-semibold'>HOME</span></Link>
+            </div>
+        </>
     );
 };
 
